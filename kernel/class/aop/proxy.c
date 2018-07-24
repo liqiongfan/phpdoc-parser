83bd9b97d4edad00f5e5a432fb7c44535ed6dde2
liqiongfan <you@example.com>
2018-07-24 09:42:56 +0800

    Fix

--- kernel/class/aop/proxy.c
+++ kernel/class/aop/proxy.c
@@ -65,7 +65,7 @@ ZEND_END_ARG_INFO()
 /**
  * {{{ proto AopProxy::ZEND_CONSTRUCTOR_FUNC_NAME()
  */
-XAN_METHOD(AopProxy, ZEND_CONSTRUCTOR_FUNC_NAME)
+XAN_METHOD(AopProxy, __construct)
 { }
 /*}}}*/
 
@@ -97,7 +97,7 @@ XAN_METHOD(AopProxy, instance)
     zend_update_property_str(XAN_ENTRY_OBJ(return_value), XAN_STRL(CLASS_CE), class_name);
 }/*}}}*/
 
-XAN_METHOD(AopProxy, ZEND_CALL_FUNC_NAME)
+XAN_METHOD(AopProxy, __call)
 {
     zval *parameters;
     zend_string *function_name;
@@ -117,7 +117,7 @@ XAN_METHOD(AopProxy, ZEND_CALL_FUNC_NAME)
  * {{{
  * proto AopProxy::ZEND_SET_FUNC_NAME($attrName, $attrValue)
  */
-XAN_METHOD(AopProxy, ZEND_SET_FUNC_NAME)
+XAN_METHOD(AopProxy, __set)
 {
     zend_string *key;
     zval *value, class_obj, z_key;
@@ -155,7 +155,7 @@ XAN_METHOD(AopProxy, ZEND_SET_FUNC_NAME)
 /**
  * {{{ proto AopProxy::ZEND_GET_FUNC_NAME($attrName)
  */
-XAN_METHOD(AopProxy, ZEND_GET_FUNC_NAME)
+XAN_METHOD(AopProxy, __get)
 {
     zval class_obj, z_key;
     zend_string *key;
@@ -189,11 +189,11 @@ XAN_METHOD(AopProxy, ZEND_GET_FUNC_NAME)
 }/*}}}*/
 
 XAN_FUNCTIONS(aop_proxy)
-    XAN_ME(AopProxy, ZEND_CONSTRUCTOR_FUNC_NAME, arginfo_xan_aop_proxy_construct, ZEND_ACC_PRIVATE)
+    XAN_ME(AopProxy, __construct, arginfo_xan_aop_proxy_construct, ZEND_ACC_PRIVATE)
     XAN_ME(AopProxy, instance, arginfo_xan_aop_proxy_instance, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
-    XAN_ME(AopProxy, ZEND_CALL_FUNC_NAME, arginfo_xan_aop_proxy_call, ZEND_ACC_PUBLIC)
-    XAN_ME(AopProxy, ZEND_GET_FUNC_NAME, arginfo_xan_aop_proxy_get, ZEND_ACC_PUBLIC | ZEND_ACC_FINAL)
-    XAN_ME(AopProxy, ZEND_SET_FUNC_NAME, arginfo_xan_aop_proxy_set, ZEND_ACC_PUBLIC | ZEND_ACC_FINAL)
+    XAN_ME(AopProxy, __call, arginfo_xan_aop_proxy_call, ZEND_ACC_PUBLIC)
+    XAN_ME(AopProxy, __get, arginfo_xan_aop_proxy_get, ZEND_ACC_PUBLIC | ZEND_ACC_FINAL)
+    XAN_ME(AopProxy, __set, arginfo_xan_aop_proxy_set, ZEND_ACC_PUBLIC | ZEND_ACC_FINAL)
 XAN_FUNCTIONS_END()
 
 /**
